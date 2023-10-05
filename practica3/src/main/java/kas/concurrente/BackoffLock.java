be85c41989aba2ed682b9d2d43f54b4e61cfc14e
arielmerinos <arielmerino@ciencias.unam.mx>
2023-10-04 18:03:24 -0600

    A first approach to the min and max backofflock algth

--- practica3/src/main/java/kas/concurrente/BackoffLock.java
+++ practica3/src/main/java/kas/concurrente/BackoffLock.java
@@ -1,17 +1,51 @@
 package kas.concurrente;
 
-public class BackoffLock implements Lock {
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicBoolean;
 
-    @Override
-    public void lock() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'lock'");
+public class BackoffLock implements Lock {
+    private AtomicBoolean state = new AtomicBoolean(false);
+    private static final int MIN_DELAY = 1;
+    private static final int MAX_DELAY = 1000; 
+    
+    private class Backoff {
+        final int minDelay, maxDelay;
+        int limit;
+        final Random random;
+
+        public Backoff(int min, int max) {
+            minDelay = min;
+            maxDelay = max;
+            limit = minDelay;
+            random = new Random();
+        }
+
+        public void backoff() throws InterruptedException {
+            int delay = random.nextInt(limit);
+            limit = Math.min(maxDelay, 2 * limit);
+            Thread.sleep(delay);
+        }
     }
 
     @Override
+    public void lock() {
+        Backoff backoff = new Backoff(MIN_DELAY, MAX_DELAY);
+        while (true) {
+            while (state.get()) {} // Espera activamente si el bloqueo está adquirido
+            if (!state.getAndSet(true)) { // Intenta adquirir el bloqueo si está libre
+                return;
+            } else {
+                try {
+                    backoff.backoff(); // Retrocede si no pudo adquirir el bloqueo
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }    }
+
+    @Override
     public void unlock() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'unlock'");
+        state.set(false); // Libera el bloqueo
     }
     
 }
