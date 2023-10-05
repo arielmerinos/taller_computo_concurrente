f2c74447d6f119c529a783aa5cdf2ee24d84cdf7
arielmerinos <arielmerino@ciencias.unam.mx>
2023-10-05 01:02:54 -0600

    MCSLock is working

--- practica3/src/main/java/kas/concurrente/MCSLock.java
+++ practica3/src/main/java/kas/concurrente/MCSLock.java
@@ -3,6 +3,7 @@ package kas.concurrente;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class MCSLock implements Lock {
+
     AtomicReference<QNode> tail;
     ThreadLocal<QNode> myNode;
 
@@ -22,16 +23,14 @@ public class MCSLock implements Lock {
         
         QNode qnode = myNode.get();
         QNode pred = tail.getAndSet(qnode);
-        
         if (pred != null) {
             System.out.println(Thread.currentThread().getName() + " - Hay un predecesor. Esperando...");
             qnode.locked = true;
             pred.next = qnode;
-            
             // Espera activamente hasta que el predecesor libere el bloqueo
             while (qnode.locked) {}
         }
-
+        
         System.out.println(Thread.currentThread().getName() + " - Bloqueo adquirido.");
     }
 
@@ -41,24 +40,25 @@ public class MCSLock implements Lock {
         
         QNode qnode = myNode.get();
         if (qnode.next == null) {
-            System.out.println(Thread.currentThread().getName() + " - No hay sucesor. Intentando liberar tail...");
             if (tail.compareAndSet(qnode, null)) {
-                System.out.println(Thread.currentThread().getName() + " - Bloqueo liberado sin sucesores.");
+                System.out.println(Thread.currentThread().getName() + " - No hay sucesor. Liberando tail...");
                 return;
             }
             // Espera hasta que el predecesor llene su campo next
             System.out.println(Thread.currentThread().getName() + " - Esperando a que el sucesor llene su campo next...");
             while (qnode.next == null) {}
         }
+        
         System.out.println(Thread.currentThread().getName() + " - Notificando al sucesor para que adquiera el bloqueo...");
         qnode.next.locked = false;
         qnode.next = null;
+
         System.out.println(Thread.currentThread().getName() + " - Bloqueo liberado y sucesor notificado.");
     }  
 
     class QNode {
-        boolean locked = false;
-        QNode next = null;
+        volatile boolean locked = false;
+        volatile QNode next = null;
     }
     
 }
