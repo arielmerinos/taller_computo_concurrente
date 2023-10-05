f44aab0186095e58a3a9a65802860771d78977ae
arielmerinos <arielmerino@ciencias.unam.mx>
2023-10-04 19:06:24 -0600

    stack based method is complete

--- practica3/src/main/java/kas/concurrente/MCSLock.java
+++ practica3/src/main/java/kas/concurrente/MCSLock.java
@@ -1,17 +1,48 @@
 package kas.concurrente;
 
-public class MCSLock implements Lock {
+import java.util.concurrent.atomic.AtomicReference;
 
+
+public class MCSLock implements Lock {
+    AtomicReference<QNode> tail;
+    ThreadLocal<QNode> myNode;
+
+    public MCSLock() {
+        tail = new AtomicReference<QNode>(null);
+        myNode = new ThreadLocal<QNode>() {
+            protected QNode initialValue() {
+                return new QNode();
+            }
+        };
+    }
+    
     @Override
     public void lock() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'lock'");
+        QNode qnode = myNode.get();
+        QNode pred = tail.getAndSet(qnode);
+        if (pred != null) {
+            qnode.locked = true;
+            pred.next = qnode;
+            // Espera activamente hasta que el predecesor libere el bloqueo
+            while (qnode.locked) {}
+        }
     }
 
     @Override
     public void unlock() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'unlock'");
+        QNode qnode = myNode.get();
+        if (qnode.next == null) {
+            if (tail.compareAndSet(qnode, null)) return;
+            // Espera hasta que el predecesor llene su campo next
+            while (qnode.next == null) {}
+        }
+        qnode.next.locked = false;
+        qnode.next = null;
+    }  
+
+    class QNode {
+        boolean locked = false;
+        QNode next = null;
     }
     
 }
