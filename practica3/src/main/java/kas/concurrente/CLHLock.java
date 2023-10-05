7bac0240a9ef55040e1f5213199885ac5aadb217
arielmerinos <arielmerino@ciencias.unam.mx>
2023-10-04 18:49:48 -0600

    progress until clhlock achieved

--- practica3/src/main/java/kas/concurrente/CLHLock.java
+++ practica3/src/main/java/kas/concurrente/CLHLock.java
@@ -1,7 +1,30 @@
 package kas.concurrente;
 
-public class CLHLock implements Lock {
+import java.util.concurrent.atomic.AtomicReference;
 
+public class CLHLock implements Lock {
+    class QNode {
+        boolean locked = false;
+    }
+
+    AtomicReference<QNode> tail;
+    ThreadLocal<QNode> myPred;
+    ThreadLocal<QNode> myNode;
+
+    public CLHLock() {
+        tail = new AtomicReference<QNode>(null);
+        myNode = new ThreadLocal<QNode>() {
+            protected QNode initialValue() {
+                return new QNode();
+            }
+        };
+        myPred = new ThreadLocal<QNode>() {
+            protected QNode initialValue() {
+                return null;
+            }
+        };
+    }
+    
     @Override
     public void lock() {
         // TODO Auto-generated method stub
